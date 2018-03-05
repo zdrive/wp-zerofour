0f526567c26e07e4ee62bb248d0c614fed67a544
Tim Heffley <dev@zdcs.com>
2018-03-04 20:56:35 -0800

    Added 404 content (Issue #22)

--- 404.php
+++ 404.php
@@ -1,38 +1,48 @@
 <?php
 /**
- * Template Name: Full Width Page
+ * The template for displaying 404 pages (not found).
  *
  * @package WordPress
  * @subpackage WP-ZeroFour
  * @since WP-ZeroFour 1.0
  */
- 
+
+	// global $wp04_theme_options;
+	$wp04_theme_options = get_option( 'wp04_theme_options' );
+	$error404_image = $wp04_theme_options[ 'error404_image' ];
+	$error404_heading = $wp04_theme_options[ 'error404_heading' ];
+	$error404_subheading = $wp04_theme_options[ 'error404_subheading' ];
+	$error404_content = $wp04_theme_options[ 'error404_content' ];
+	// $error404_ = $wp04_theme_options[ 'error404_' ];
+
+$wp04_demo_mode = true; 
+
+if ($wp04_demo_mode){
+	if (trim($error404_image) == ""){$error404_image = "/wp-content/themes/wp-zerofour-master/images/stock/404-error-warning-sign_1200x600.jpg";}
+	if (trim($error404_heading) == ""){$error404_heading = "Page Not Found";}
+	if (trim($error404_subheading) == ""){$error404_subheading = "Error 404";}
+	if (trim($error404_content) == ""){$error404_content = '<p style="text-align:center;">Error 404 &mdash; Page Not Found</p>';}
+} // END if ($wp04_demo_mode)
+
 get_header(); ?>
 				<div class="main-wrapper-style2">
 					<div class="inner">
 						<div class="container">
 							<div class="row">
 								<div class="12u skel-cell-important">
-									<div id="content">
-<?php
-	// Start the Loop.
-	while ( have_posts() ) : the_post();
-		// Include the page content template.
-		get_template_part( 'content', 'page' );
-
-		// If comments are open or we have at least one comment, load up the comment template.
-		if ( comments_open() || get_comments_number() ) {
-			comments_template();
-		}
-	endwhile;
-?>
+									<div id="content" style="text-align:center;">
+										<header class="page-header">
+											<h2><?php echo $error404_heading; ?></h2>
+											<p style="text-align:center; width: 100%; margin: auto;"><span class="image featured"><img src="<?php echo $error404_image; ?>" alt="<?php echo $error404_heading; ?> <?php echo $error404_subheading; ?>" /></span>
+											<h3><?php echo $error404_subheading; ?></h3></p>
+											<?php echo $error404_content; ?>
+										</header>
 									</div>  <!-- #content -->
 								</div>  <!-- .12u.skel-cell-important -->
 							</div>  <!-- .row -->
 						</div>  <!-- .container -->
 					</div>  <!-- .inner -->
 				</div>  <!-- .main-wrapper-style2 -->
-<?php get_template_part( 'loop', 'recent-posts' ); ?>
-			</div>
+			</div><!-- .main-wrapper -->
 
 <?php get_footer(); ?>
